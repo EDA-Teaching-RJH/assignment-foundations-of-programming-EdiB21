898332482fdd5bfdb574653c0e7e3a8d9187a4a2
EdiB21 <eb614@kent.ac.uk>
2026-02-11 15:51:32 +0000

    Bug three

--- old_system.py
+++ old_system.py
@@ -12,8 +12,8 @@ def run_system_monolith():
     
     loading = 0
     while loading < 5:
-        print("Loading module " + str(loading))
-        
+        print("Loading module " + str(loading + 1) + "...")
+        loading += 1
     
     while True:
         print("\n--- MENU ---")
