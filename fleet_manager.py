e41cfed29b50b794b614babe16526dc878a96a28
EdiB21 <eb614@kent.ac.uk>
2026-02-11 21:44:46 +0000

    Function One

---
+++ fleet_manager.py
@@ -0,0 +1,8 @@
+def init_database():
+    global N, R, D, ID
+    N = ["Picard", "Riker", "Data", "Forge", "Crusher"]
+    R = ["Captain", "Commander", "Lt.Commander", "Lt.Commander", "Commander"]
+    D = ["Command", "Command", "Operations", "Operations", "Sciences"]
+    ID = ["001", "002", "003", "004", "005"]
+    return N, R, D, ID
+
